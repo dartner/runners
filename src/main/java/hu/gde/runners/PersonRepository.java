1676a9dbbe8bcc87c5963735baee3771a36fe77d
dartner <daniel.artner10@gmail.com>
2024-12-01 17:30:03 +0100

    Ping data access with h5

---
+++ src/main/java/hu/gde/runners/PersonRepository.java
@@ -0,0 +1,10 @@
+package hu.gde.runners;
+
+import org.springframework.data.repository.CrudRepository;
+
+// This will be AUTO IMPLEMENTED by Spring into a Bean called userRepository
+// CRUD refers Create, Read, Update, Delete
+
+public interface PersonRepository extends CrudRepository<Person, Integer> {
+
+}
